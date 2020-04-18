3a9dc78b54e9895c4235f970c9263d1e1bf0b3a7
CiaoLee <362692489@qq.com>
2020-04-18 20:11:53 +0800

    20.04.18-0 页面访问操作记录初步

--- src/main/java/com/yaqiu/util/VisitorUtil.java
+++ src/main/java/com/yaqiu/util/VisitorUtil.java
@@ -29,16 +29,12 @@ public class VisitorUtil {
         Browser browser = userAgent.getBrowser();
         //获取操作系统对象
         OperatingSystem operatingSystem = userAgent.getOperatingSystem();
+        System.out.println("访问设备类型:"+operatingSystem.getDeviceType());
         System.out.println("浏览器名:"+browser.getName());
-        System.out.println("浏览器类型:"+browser.getBrowserType());
         System.out.println("浏览器家族:"+browser.getGroup());
-        System.out.println("浏览器生产厂商:"+browser.getManufacturer());
-        System.out.println("浏览器使用的渲染引擎:"+browser.getRenderingEngine());
         System.out.println("浏览器版本:"+userAgent.getBrowserVersion());
         System.out.println("操作系统名:"+operatingSystem.getName());
-        System.out.println("访问设备类型:"+operatingSystem.getDeviceType());
         System.out.println("操作系统家族:"+operatingSystem.getGroup());
-        System.out.println("操作系统生产厂商:"+operatingSystem.getManufacturer());
         return null;
     }
 }
