bc9eed8120c3041a7d312cb4a3fd6ee77fb44eba
CiaoLee <362692489@qq.com>
2020-05-09 13:45:02 +0800

    20.05.09.0 加入百度验证文件; InsuranceInterceptor中增加验证文件放行规则;

--- src/main/java/com/yaqiu/interceptor/InsuranceInterceptor.java
+++ src/main/java/com/yaqiu/interceptor/InsuranceInterceptor.java
@@ -12,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 public class InsuranceInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        /* 百度站点验证文件 */
+        if("/baidu_verify_LL4bi1KNwx.html".equals(uri)) return true;
+        /* 搜狗站点验证工具 */
+        /* 360站点验证工具 */
         return false;
     }
 }
